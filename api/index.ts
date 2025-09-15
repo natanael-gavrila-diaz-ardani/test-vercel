98ae35ea482603f8256ad288f6b8cc9fc8f31cd4
Natanael <nathanaelsltg123@gmail.com>
2025-09-15 22:33:29 +0700

    update vercel.json and index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,15 +1,15 @@
-// import { VercelRequest, VercelResponse } from "@vercel/node";
-// import app from "../src/app";
-// import sequelize from "../src/config/database";
-//
-// export default async function handler(req: VercelRequest, res: VercelResponse) {
-//     // try {
-//     //     await sequelize.authenticate();
-//     //     console.log("Database connected");
-//     // } catch (err) {
-//     //     console.error("DB connection failed", err);
-//     // }
-//     //
-//     // return app(req, res);
-//     return app(req, res);
-// }
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../src/app";
+import sequelize from "../src/config/database";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+    try {
+        await sequelize.authenticate();
+        console.log("Database connected");
+    } catch (err) {
+        console.error("DB connection failed", err);
+    }
+    
+    return app(req, res);
+    // return app(req, res);
+}
