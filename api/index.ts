8286f30466cfee423908e37251a403419ea49259
Pramadha <pramslabs@gmail.com>
2025-09-15 17:32:37 +0700

    fix: fix vercel 2

--- api/index.ts
+++ api/index.ts
@@ -3,12 +3,13 @@ import app from "../src/app";
 import sequelize from "../src/config/database";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    try {
-        await sequelize.authenticate();
-        console.log("Database connected");
-    } catch (err) {
-        console.error("DB connection failed", err);
-    }
-
+    // try {
+    //     await sequelize.authenticate();
+    //     console.log("Database connected");
+    // } catch (err) {
+    //     console.error("DB connection failed", err);
+    // }
+    //
+    // return app(req, res);
     return app(req, res);
 }
